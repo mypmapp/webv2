5c9c162278599647d653b5cddb3815a2ea183404
sri@mypmnode <mypmnode@gmail.com>
2015-10-02 19:18:19 +0100

    Property Lookup Using Shared Scope

--- public/modules/properties/directives/property-lookup-name.client.directive.js
+++ public/modules/properties/directives/property-lookup-name.client.directive.js
@@ -1,38 +1,48 @@
 'use strict';
 
-angular.module('properties').directive('propertyLookupName', ['$http', function(http) {
-  return {
-    template: "<span>{{name}}</span>",
-    scope: {
-      value: '=',
-      templateOptions:'=to'
-    },
-    link: function(scope) {
-      // console.log(scope.value);
-      // console.log(scope.templateOptions.lookup);
-      var urlLink='';
-      switch (scope.templateOptions.lookup) {
-        case 'chain':
-          urlLink = '/lookup/chainTypes/' + scope.value;
-          break;
-        case 'property':
-          urlLink = '/lookup/propertyTypes/' + scope.value;
-          break;
-        case 'tenure':
-          urlLink = '/lookup/tenureTypes/' + scope.value;
-          break;
-        default:
-          scope.name ='';
-      }
-      
-      if(urlLink)
-      {
-          http.get(urlLink).success(function(data) {
-          scope.name = data.name;
-        }, function(err) {
-          scope.name = '';
-        });
-      }
+angular.module('properties').directive('propertyLookupName', ['$http', function (http) {
+    return {
+        template: "<span>{{name}}</span>",
+        link: function (scope,element, attributes) {
+            console.log(attributes.lookUp);
+            console.log(scope.property);
+
+            var urlLink = '';
+            scope.name = '';
+
+            attributes.$observe('propertyLookupName', function(value){
+                console.log(value);
+            });
+
+            attributes.$observe('lookUp', function(value){
+                console.log(value);
+            });
+
+            switch (attributes.lookUp) {
+                case 'chain':
+                    scope.property.chainType ? urlLink = '/lookup/chainTypes/' + scope.property.chainType
+                        : scope.name = '';
+                    break;
+                case 'property':
+                    scope.property.propertyType ?
+                        urlLink = '/lookup/propertyTypes/' + scope.property.propertyType
+                        : scope.name = '';
+                    break;
+                case 'tenure':
+                    scope.property.tenureType ? urlLink = '/lookup/tenureTypes/' + scope.property.tenureType
+                        : scope.name = '';
+                    break;
+                default:
+                    scope.name = '';
+            }
+
+            if (urlLink) {
+                http.get(urlLink).success(function (data) {
+                    scope.name = data.name;
+                }, function (err) {
+                    scope.name = '';
+                });
+            }
+        }
     }
-  }
 }]);
